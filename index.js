9550bc1ae72db22105cd490b3b924cf96b4e7534
ysjinxsy <168466901+ysjinxsy@users.noreply.github.com>
2024-04-29 10:18:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let client = new Discord.Client()
 
 
 client.on('ready', () => {
- client.user.setActivity(`1.0.1 your prefix here`, { type: 'PLAYING'})
+ client.user.setActivity(`ROBLOX`, { type: 'PLAYING'})
 })
 
 client.login(process.env.token);
